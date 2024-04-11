b13d98649749e1886fc08cfd63f16f4e7b76cfd3
habib16gb <habib16gb@gmail.com>
2024-04-11 11:23:16 +0100

    add first Async Thunk

---
+++ client/src/store/categories/actions/actionGetCategories.ts
@@ -0,0 +1,23 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const actionGetCategories = createAsyncThunk(
+  "categories/actionGetCategories",
+  async (_, thunkApi) => {
+    const { rejectWithValue } = thunkApi;
+    try {
+      const response = await axios.get(
+        "https://fakestoreapi.com/products/categories"
+      );
+      return response.data;
+    } catch (error) {
+      if (error instanceof axios.AxiosError) {
+        return rejectWithValue(error.response?.data.message || error.message);
+      } else {
+        return rejectWithValue("An unknown error occurred");
+      }
+    }
+  }
+);
+
+export default actionGetCategories;
